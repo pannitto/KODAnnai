e510943599e5d0ad10e278ed31cfd76aa845f817
東山秀行 <2125206x@g.hit-u.ac.jp>
2026-06-08 13:54:37 +0900

    match light banner aspect ratio to dark banner

--- app/home-page-client.tsx
+++ app/home-page-client.tsx
@@ -659,7 +659,7 @@ export default function HomePageClient() {
           aria-label="KODAIRA祭公式サイトはこちらから"
         >
           {/* ライトモード用 */}
-          <svg xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" viewBox="0 0 1200 200" className="w-full h-auto dark:hidden">
+          <svg xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" viewBox="0 0 1200 200" className="w-full h-auto dark:hidden" style={{ aspectRatio: '1416.97 / 215.64' }}>
             <defs>
               <style>{`
                 .cls-1 { letter-spacing: .25em; }
